a687f38b199740f013bb4c9818cccecef04b8029
nqgiakhanh <62598007+nqgiakhanh@users.noreply.github.com>
2020-03-24 18:08:17 +0700

    feature/html

---
+++ login/login.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+		<p> LOGIN</p>
+		<form action="login.php" method="post">
+			<input type="text" name="email">
+			<input type="pass" name="password">
+			<input type="submit" name="submit">
+		</form>
+</body>
+</html>
