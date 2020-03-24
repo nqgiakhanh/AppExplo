ab84e8bf9984cdc9e03feea1a38ef8feac085685
nqgiakhanh <62598007+nqgiakhanh@users.noreply.github.com>
2020-03-24 22:55:09 +0700

    feature/html

---
+++ inc/Footer.php
@@ -0,0 +1,50 @@
+<div class="header-contact">
+        <div class="row" style="margin-left: 115px; margin-right: 50px; padding: 0;">
+            <div class="col-sm-3">
+                <a class="navbar-brand font-weight-bold text-dark mb-3" href="#"><img src="/img/logo.svg" width="37" alt="">AppExplode</a>
+                <div style=" font-size: 15px;">
+                    <p class="font-weight-bold">Business Hours</p>
+                    <p>Monday - Friday 08:00AM to 05:00PM</p>
+                    <p style="margin-top: -10px;">Saturday - Closed</p>
+                    <p style="margin-top: -10px;">Sunday - Closed</p>
+                </div>
+                <div class="icons-brand">
+                    <i class="fab fa-facebook-square mr-3" title="Facebook"></i>
+                    <i class="fab fa-instagram mr-3" title="Instagram"></i>
+                    <i class="fab fa-twitter mr-3" title="Twitter"></i>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <a class="navbar-brand font-weight-bold text-dark mb-3 ">Contact</a>
+                <div class="content-contact">
+                    <i class="fas fa-envelope mb-3" style="margin-top: 45px;"><span class="ml-3 font-weight-normal">team@appexplode.com</span></i>
+                    <i class="fas fa-phone mb-3"><span class="ml-3 font-weight-normal">LLOYD +61 40 444 1243</span></i>
+                    <i class="fas fa-phone"><span class="ml-3 font-weight-normal">PAM +61 420 922 215</span></i>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <a class="navbar-brand font-weight-bold text-dark mb-3">Company</a>
+                <p style="margin-top: 40px;">Who we are</p>
+                <p>How it work</p>
+                <p>Team</p>
+                <p>Instant Quote</p>
+            </div>
+            <div class="col-sm-3">
+                <a class="navbar-brand font-weight-bold text-dark mb-4">Help</a>
+                <p style="margin-top: 40px;">Terms and Conditions</p>
+                <p>Privacy Policy</p>
+            </div>
+        </div>
+    </div>>
+    <p style="margin-left: 130px; margin-top: 10px;">Appexplode, 2019, All rights reserved.</p>
+    <div class="row" style="padding: 0; margin: 0;">
+        <div class="col">
+            <p class="font-italic" style="margin-left: 115px; margin-top: 30px;">*We dont's share your personal info or idea with anyone. We are happy to sign on NDA before full of an idea. Check out our Privacy for more infomation.</p>
+        </div>
+        <div class="col">
+
+        </div>
+    </div>
+    </body>
+
+</html>
